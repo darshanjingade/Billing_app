4f351e48a7cd9145995b123572e78ae2bf38801e
Darshan Jayakumar Jingade <jdarshan@zeomega.com>
2023-10-02 20:46:23 +0530

    update

--- js/loginService.js
+++ js/loginService.js
@@ -1,3 +1,24 @@
-getLoginCridentiels : function(){
-    return $http.get("Billing_app/python/loginData");
-}
+// function getLoginCridentiels(param){
+//     debugger;
+//     return $http.get("Billing_app/python/login_crdentiels_check/loginData");
+// }
+
+// // var app = angular.module('BillingApp',[])
+// // app.controller('loginService', function($http) {
+// getLoginCridentiels = function(param) {
+//         debugger;
+//         return $http.get('./python/login_crdentiels_check/loginData',{params:{input: param}});
+//     }
+// // })
+// // getLinesToApplyDecision: function(params) {
+// //     return $http.get("AppealEpisode/Controller/getLinesToApplyDecision", params);
+// // }
+
+var app = angular.module('BillingApp',[]);
+
+// app.service('loginService', function($http) {
+//     this.
+    getLoginCridentiels = function(param) {
+        return $http.get('./python/login_crdentiels_check/loginData',{params:{input: param}});
+    }
+// });
--- js/login_controller.js
+++ js/login_controller.js
@@ -1,17 +1,17 @@
-var app = angular.module('BillingApp', []);
-
 app.controller('LoginController', function () {
     var ls = this;
     ls.username = '';
     ls.password = '';
-    ls.loginData = function(){
-        loginService.getLoginCridentiels()
+    ls.data = {}
+    ls.loginData = function(param){
+        getLoginCridentiels(param)
         .then(function(response){
-            
+            ls.data = response.data;
         })
     }
 
     ls.onSubmit = function () {
+        ls.loginData(ls.username)
         if (ls.username === 'your_username' && ls.password === 'your_password') {
             alert('Login successful!');
             // Redirect to the dashboard or another page as needed.
